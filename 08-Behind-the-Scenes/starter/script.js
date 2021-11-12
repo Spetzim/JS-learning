9b6e7f27c94243abb2bcb624f5d68f7bb7b418f3
Spetzim <Besim.musliu94@hotmail.com>
2021-11-12 13:36:42 +0100

    primitive vs reference

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -166,3 +166,22 @@ var addArrow = (a, b) => {
 }
 addArrow(2, 5, 8);
 */
+
+/*
+// PRIMITIVE VS. REFERENCE VALUES
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+    name: 'Besim',
+    age: 30,
+};
+
+const friend = me;
+friend.age = 27;
+console.log('Friend', friend);
+console.log('Me', me);
+*/
