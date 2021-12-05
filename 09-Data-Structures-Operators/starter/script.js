00a24441f59eeb48d1d3482d827f0d943d59304e
Spetzim <Besim.musliu94@hotmail.com>
2021-12-05 16:05:17 +0100

    SETS

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -53,6 +53,47 @@ const restaurant = {
   }
 };
 
+//MAPS
+
+
+
+// SETS
+/*
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza'
+]);
+console.log(ordersSet);
+console.log(new Set('Jonas'));
+console.log(new Set());
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic bread');
+ordersSet.add('Garlic bread');
+ordersSet.delete('Risotto')
+console.log(ordersSet);
+
+for (const order of ordersSet) {
+  console.log(order);
+}
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+
+console.log(new Set('BesimMusliu').size);
+*/
+
 /*
 // Property NAMES
 const properties = Object.keys(openingHours)
