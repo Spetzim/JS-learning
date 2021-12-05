4ff5d9a8b45866c8aa5a70691f53064ef00ca5ec
Spetzim <Besim.musliu94@hotmail.com>
2021-12-05 17:41:05 +0100

    codingchallenge#3

--- 09-Data-Structures-Operators/starter/codingChallenge.js
+++ 09-Data-Structures-Operators/starter/codingChallenge.js
@@ -80,7 +80,6 @@ team1 < team2 && console.log('team 1 is more likely to win');
 team2 < team1 && console.log('team 2 is more likely to win')
 */
 
-
 // CODINGCHALLENGE #2
 /*
 
@@ -162,3 +161,36 @@ console.log(scorers);
 */
 
 // CODINGCHALLENGE #3
+/*
+const gameEvents = new Map([
+    [17, '⚽️ GOAL'],
+    [36, '🔁 Substitution'],
+    [47, '⚽️ GOAL'],
+    [61, '🔁 Substitution'],
+    [64, '🔶 Yellow card'],
+    [69, '🔴 Red card'],
+    [70, '🔁 Substitution'],
+    [72, '🔁 Substitution'],
+    [76, '⚽️ GOAL'],
+    [80, '⚽️ GOAL'],
+    [92, '🔶 Yellow card'],
+]);
+
+// 1.
+const events = [...new Set(gameEvents.values())]
+console.log(events);
+
+// 2.
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3.
+const time = [...gameEvents.keys()].pop()
+console.log(`An event happened, on average, every ${time / gameEvents.size} minutes`);
+
+// 4.
+for (const [minute, happened] of gameEvents) {
+    const str = minute <= 45 ? '[FIRST HALF]' : '[SECOND HALF]'
+    console.log(`${str} ${minute}: ${happened}`)
+}
+*/
