72f3a69f8078f3136c727316f7511109c9f8d73d
Spetzim <Besim.musliu94@hotmail.com>
2021-12-05 15:41:12 +0100

    codingchallenge #2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -53,12 +53,36 @@ const restaurant = {
   }
 };
 
-if (restaurant.openingHours && restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+/*
+// Property NAMES
+const properties = Object.keys(openingHours)
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `
+}
+console.log(openStr);
 
 
+
+// Property VALUES
+const values = Object.values(openingHours)
+console.log(values);
+
+// Entire object
+const entries = Object.entries(openingHours)
+//console.log(entries);
+
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+*/
 
 /*
-///////// OPTIONAL CHAING AND NULLISH <3
+///////// OPTIONAL CHAINING AND NULLISH <3
 // WITH optional chaining
 console.log(restaurant.openingHours.mon?.open);
 console.log(restaurant.openingHours?.mon?.open);
