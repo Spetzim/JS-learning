85a49d5965d1965aee165d617d0315e61910c229
Spetzim <Besim.musliu94@hotmail.com>
2021-12-14 09:45:22 +0100

    Strings

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,6 +54,107 @@ const restaurant = {
 };
 
 
+
+//STRINGS
+/*
+const fixPassangerName = function (passenger) {
+  const passengerLower = passenger.toLowerCase();
+  const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1)
+  console.log(passengerCorrect);
+}
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// Fix capitalization in name
+const passenger = 'bEsIM';
+fixPassangerName(passenger)
+
+// Comparing email
+const email = 'hello@besim.it'
+const loginEmail = '  Hello@Besim.It \n'
+
+
+const normalizedEmail = loginEmail.toLocaleLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// Replacing
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.')
+console.log(priceUS);
+
+const announcement = 'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replaceAll('door', 'gate'));
+
+console.log(announcement.replace(/door/g, 'gate'));
+
+// Booleans
+const plane = 'Airbus A320';
+
+console.log(plane.includes('A320'));
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('Part of the NEW Airbus family');
+}
+
+// Practice exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard');
+  }
+}
+checkBaggage('I have a laptop, some  Food and a pocket Knife')
+checkBaggage('Socks and camera')
+checkBaggage('Got some snacks and a agun for protection')
+*/
+
+//STRINGS
+/*
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('Portugal'));
+
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const letter = seat.slice(-1)
+  if (letter === 'B' || letter === 'E') console.log('You got the middle seat 😢');
+  else console.log('You got lucky😊');
+}
+
+
+checkMiddleSeat('11B')
+checkMiddleSeat('23C')
+checkMiddleSeat('3E')
+
+console.log(new String('Besim'));
+console.log(typeof new String('Besim'));
+*/
+
 //MAPS part 2
 /*
 
