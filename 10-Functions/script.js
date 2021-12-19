f4188b15afc8eaa379ee5e2b4f6d79734f7375fb
Spetzim <Besim.musliu94@hotmail.com>
2021-12-19 11:49:57 +0100

    Higher order functions and callbacks

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -61,3 +61,32 @@ newPassport(besim);
 checkIn(flight, besim)
 */
 
+// High Order functions
+
+const oneWord = function (str) {
+    return str.replaceAll(' ', '').toLowerCase();
+}
+
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+}
+
+// Higher-Order function
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+
+    console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('JavaScript is the best!', upperFirstWord)
+transformer('JavaScript is the best!', oneWord)
+
+// JS uses calbacks all the time
+const high5 = function () {
+    console.log('👋');
+}
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
