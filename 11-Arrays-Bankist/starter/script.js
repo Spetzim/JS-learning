00ef0662f3ac805cff30941dd9ff75a4e3c7ca08
Spetzim <Besim.musliu94@hotmail.com>
2022-01-02 18:07:13 +0100

    ---

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -75,36 +75,32 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html)
   })
 }
-displayMovements(account1.movements)
 
-const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} €`;
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance} €`;
 }
-calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, curr) => acc + curr, 0);
   labelSumIn.textContent = `${incomes}€`
 
-  const out = movements
+  const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, curr) => acc + curr, 0);
   labelSumOut.textContent = `${Math.abs(out)}€`
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov > 0)
-    .map(deposit => deposit * 1.2 / 100)
+    .map(deposit => deposit * acc.interestRate / 100)
     .filter((int, i, arr) => {
-      console.log(arr);
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
   labelSumInterest.textContent = `${interest}`
 }
-calcDisplaySummary(account1.movements)
 
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -116,6 +112,68 @@ const createUsernames = function (accs) {
   })
 };
 createUsernames(accounts)
+
+const updateUI = function (acc) {
+  displayMovements(acc.movements)
+  calcDisplayBalance(acc)
+  calcDisplaySummary(acc)
+}
+// Event handler
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  // Prevent form from submitting
+  e.preventDefault()
+
+  currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value)
+
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(' ')[0]}`
+    containerApp.style.opacity = 100;
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    // Update UI
+    updateUI(currentAccount)
+  }
+})
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const recieverAcc = accounts.find(acc => acc.username === inputTransferTo.value.toLowerCase()
+  );
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if (amount > 0 &&
+    recieverAcc &&
+    currentAccount.balance >= amount &&
+    recieverAcc?.username.toLowerCase() !== currentAccount.username) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    recieverAcc.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+})
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+
+  if (inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin) {
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username
+    );
+
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+})
 
 
 
@@ -255,7 +313,6 @@ console.log(withdrawals);
 const balance = movements.reduce((acc, curr) => acc + curr, 0)
 console.log(balance);
 */
-
 // Maximum value
 /*
 const max = movements.reduce((acc, mov) => acc > mov ? acc : mov, movements[0])
@@ -263,7 +320,7 @@ const max = movements.reduce((acc, mov) => acc > mov ? acc : mov, movements[0])
 console.log(max);
 */
 
-
+/*
 const eurToUsd = 1.1;
 // PIPELINE
 const totalDepositsUSD = movements
@@ -272,3 +329,15 @@ const totalDepositsUSD = movements
   .reduce((acc, curr) => acc + curr, 0);
 
 console.log(totalDepositsUSD);
+*/
+
+/*
+const firstWithDrawal = movements.find(mov => mov < 0)
+console.log(movements);
+console.log(firstWithDrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis')
+console.log(account);
+*/
