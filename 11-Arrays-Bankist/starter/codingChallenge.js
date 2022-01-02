88e8a45e3e282b3ceb74a3643d5df63d58a663cc
Spetzim <Besim.musliu94@hotmail.com>
2022-01-02 14:23:43 +0100

    codingchallenge#1

---
+++ 11-Arrays-Bankist/starter/codingChallenge.js
@@ -0,0 +1,28 @@
+'use strict';
+
+
+// Challenge #1
+/*
+const julia = [9, 16, 6, 8, 3]
+const kate = [10, 5, 6, 1, 4]
+
+const checkDogs = function (dogsJulia, dogsKate) {
+    //1.
+    const correctJulia = dogsJulia.slice(1, -2);
+    //2.
+    const alldogs = [...correctJulia, ...dogsKate]
+    //3.
+    alldogs.forEach(function (dog, index) {
+        if (dog < 3) {
+            console.log(`Dog number ${index + 1} is still a puppy`);
+        } else {
+            console.log(`Dog number ${index + 1} is an adult, and is ${dog} years old`);
+        }
+    })
+
+}
+
+//4.
+checkDogs(julia, kate)
+*/
+
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,25 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal'
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>`;
+    containerMovements.insertAdjacentHTML('afterbegin', html)
+  })
+}
+displayMovements(account1.movements)
+
+
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -146,6 +165,7 @@ movements.forEach(function (movement, index, array) {
 })
 */
 
+/*
 // MAP
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -164,3 +184,5 @@ console.log(currenciesUnique);
 currenciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 })
+*/
+
